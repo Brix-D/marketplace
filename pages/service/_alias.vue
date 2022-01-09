f451ed4b19a9c6b42d3118186630ccfb6709ea43
Brix-D <Shlyakhta4@gmail.com>
2022-01-09 22:36:32 +0400

    added es linter and prettier

---
+++ pages/service/_alias.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ $route.params.alias }}
+        <NuxtLink :to="{ name: 'service-alias', params: { alias: 'хуй' } }"> ХУЙ </NuxtLink>
+        <div v-for="i in 8" :key="i" class="" />
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'default',
+};
+</script>
+
+<style scoped lang="scss">
+div {
+    color: #eea135;
+}
+</style>
